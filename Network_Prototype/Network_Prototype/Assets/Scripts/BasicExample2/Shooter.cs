1067491ff764336dd66b7e45c461acb0746ff8e2
Andres Gomez <and-gome@uniandes.edu.co>
2020-11-18 13:46:30 +0900

    Implement two examples of Networking with MLAPI
    Implement examples to get a hands on with MLAPI
    https://www.youtube.com/watch?v=-nS1gqSk458
    https://www.youtube.com/watch?v=vUYi__DrxZw
    It was not possible to use AIM (deprecated), but it was replaced with a
    simple image with a sight.

---
+++ Network_Prototype/Network_Prototype/Assets/Scripts/BasicExample2/Shooter.cs
@@ -0,0 +1,32 @@
+﻿using MLAPI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Shooter : NetworkedBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!IsLocalPlayer)
+        {
+            GetComponent<FirstPersonController>().enabled = false;
+            Destroy(transform.Find("FirstPersonCharacter").gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsLocalPlayer && Input.GetMouseButtonDown(0))
+        {
+            Camera camera = GetComponentInChildren<Camera>();
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+                if (hit.transform.GetComponent<FirstPersonController>() != null)
+                    Destroy(hit.transform.gameObject);
+        }
+    }
+}
